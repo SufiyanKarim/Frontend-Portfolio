afa556cd86e4d237c80091dd8846ef79d23aa666
SufiyanKarim <sufiib165@gmail.com>
2024-10-06 23:39:35 -0700

    update code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,52 +1,85 @@
-import React, { useEffect } from 'react';
-import currencyImage1 from '../assets/currencyImage1.png';
-import adminDashboard from '../assets/project1.png';
-import allProjects from '../assets/allProjects.png';
-import project3 from '../assets/project3.avif';
-import project4 from '../assets/project4.png';
-import project5 from '../assets/project5.avif';
-import AOS from 'aos'; // Import AOS
-import 'aos/dist/aos.css'; // Import AOS styles
+import React, { useEffect } from "react";
+import DGSImage from "../assets/DGS.png";
+import DGSDashboard from "../assets/DGSDashboard.png";
+import currencyImage1 from "../assets/currencyImage1.png";
+import adminDashboard from "../assets/project1.png";
+import allProjects from "../assets/allProjects.png";
+import AOS from "aos"; // Import AOS
+import "aos/dist/aos.css"; // Import AOS styles
 
 const Projects = () => {
   useEffect(() => {
     AOS.init(); // Initialize AOS
   }, []);
 
-  // Project data including links, names, and descriptions
   const projectData = [
-    { id: 1, link: 'https://admin-dashboardsyncfusion.vercel.app', image: adminDashboard, name: 'Admin Dashboard', description: 'I can make an admin-dashboard. It is fully responsive and attractive for users.' },
-    { id: 2, link: 'https://currency-converterjs.vercel.app', image: currencyImage1, name: 'Currency Converter', description: 'I can make a currency converter app with real-time API' },
-    { id: 3, link: 'https://my-all-personal-projects.vercel.app/', image: allProjects, name: 'All Projects', description: 'All my projects are available here. You can visit and see them.' },
-    { id: 4, link: '#', image: project3, name: 'Project 4', description: 'Graphic Design' },
-    { id: 5, link: '#', image: project4, name: 'Project 5', description: 'Web Development' },
-    { id: 6, link: '#', image: project5, name: 'Project 6', description: 'Graphic Art' },
+    {
+      id: 1,
+      link: "https://digital-gateway-service.vercel.app/",
+      image: DGSImage,
+      name: "Gigital Gateway Service",
+      description:
+        "I can make a digital gateway service website to provide the service for users. It is fully responsive and fully animated.",
+    },
+    {
+      id: 2,
+      link: "https://digital-gateway-service.vercel.app/",
+      image: DGSDashboard,
+      name: "Gigital Gateway Service Dashboard",
+      description:
+        "I can make a digital gateway service dashboard. Admin can add, delete, and update users and perform all activities in the dashboard.",
+    },
+    {
+      id: 3,
+      link: "https://admin-dashboardsyncfusion.vercel.app",
+      image: adminDashboard,
+      name: "Admin Dashboard",
+      description:
+        "I can make an admin dashboard. It is fully responsive and attractive for users.",
+    },
+    {
+      id: 4,
+      link: "https://currency-converterjs.vercel.app",
+      image: currencyImage1,
+      name: "Currency Converter",
+      description: "I can make a currency converter app with real-time API.",
+    },
+    {
+      id: 5,
+      link: "https://my-all-personal-projects.vercel.app/",
+      image: allProjects,
+      name: "All Projects",
+      description:
+        "All my projects are available here. You can visit and see them.",
+    },
   ];
 
   return (
     <section id="projects">
       <div className="max-w-[90%] mx-auto pt-16 md:pt-10 px-4 lg:px-8">
-        <h2 className="text-3xl md:text-4xl font-bold text-center mb-4">My Projects</h2>
-        <p className="text-center mb-12">Showcase of my works on website development</p>
+        <h2 className="text-3xl md:text-4xl font-bold text-center mb-4">
+          My Projects
+        </h2>
+        <p className="text-center mb-12">
+          Showcase of my works on website development
+        </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
           {projectData.map((project) => (
             <a
               key={project.id}
               href={project.link}
               target="_blank"
-              rel="noopener noreferrer" // Added for security when opening in a new tab
+              rel="noopener noreferrer"
               className="relative block h-48 sm:h-64 rounded-lg overflow-hidden group shadow-lg"
-              data-aos="fade-up" // AOS effect
-              data-aos-duration="1000" // Duration of the animation
+              data-aos="fade-up"
+              data-aos-duration="1000"
             >
-              {/* Background image with hover effect */}
-              <div
-                className="absolute inset-0 bg-cover bg-center transition-transform duration-500 ease-in-out transform group-hover:scale-105"
-                style={{ backgroundImage: `url(${project.image})` }}
-              ></div>
-
-              {/* Dark overlay and text reveal */}
-              <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 transition-opacity duration-500 group-hover:opacity-100 flex items-center justify-center">
+              <img
+                src={project.image}
+                alt="Project"
+                className="h-full w-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-gray-900 bg-opacity-80 opacity-0 transition-opacity duration-500 group-hover:opacity-100 flex items-center justify-center">
                 <div className="text-center text-white px-4 py-2">
                   <h3 className="text-xl font-bold mb-2">{project.name}</h3>
                   <p>{project.description}</p>
@@ -56,7 +89,6 @@ const Projects = () => {
           ))}
         </div>
 
-        {/* Button to view all projects */}
         <div className="flex justify-center">
           <a
             href="https://my-all-personal-projects.vercel.app/"
